6a328c2620acc15ce9a14dfddc0f845f9f4a6d00
Chithaara B Avinuty <23ucs010@avinuty.ac.in>
2026-01-06 09:01:18 -0800

    Temporarily hide services accessibility announcement

--- src/pages/ServicesWrapper.jsx
+++ src/pages/ServicesWrapper.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 import Services from "./Services";
-// import { announcePage } from "../utils/accessibility";
+import { announcePage } from "../utils/accessibility";
 
 export default function ServicesWrapper() {
   useEffect(() => {
-    // Accessibility announcement temporarily disabled
+    // Commented out for now, will announce later
     // announcePage(
     //   "Services page loaded. Use Tab to navigate through the service cards."
     // );
